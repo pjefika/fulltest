3ace5be2a1686d6a0b99947fd3b80d22dec770e5
henmerlin <henmerlin@gmail.com>
2017-01-27 18:18:17 -0200

    MongoDBClient OK

--- src/dao/dslam/nosql/DslamDAO.java
+++ src/dao/dslam/nosql/DslamDAO.java
@@ -5,19 +5,53 @@
  */
 package dao.dslam.nosql;
 
+import com.google.gson.Gson;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.MongoClient;
 import javax.ejb.Stateless;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import model.entity.TesteCliente;
 
 @Stateless
 public class DslamDAO {
 
-    @PersistenceContext(unitName = "fulltestPU")
-    private EntityManager em;
+    private MongoClient mongoClient;
+
+    private DB db;
+
+    private DBCollection collection;
+
+    public DslamDAO() {
+        this.mongoClient = new MongoClient("10.200.35.67", 27017);
+        this.db = mongoClient.getDB("fulltest");
+        this.collection = db.getCollection("testes");
+    }
 
     public void salvar(TesteCliente t) {
-        em.persist(t);
+        BasicDBObject basicDBObject = new BasicDBObject();
+        Gson gson = new Gson();
+        String result = gson.toJson(t);
+
+        basicDBObject.put("name", "teste");
+        basicDBObject.put("teste", result);
+
+        collection.insert(basicDBObject);
+    }
+
+    public void listar() {
+
+        BasicDBObject whereQuery = new BasicDBObject();
+        whereQuery.put("name", "teste");
+
+        DBCursor cursor = collection.find(whereQuery);
+        // DBCursor cursor = collection.find();
+
+        while (cursor.hasNext()) {
+            System.out.println(cursor.next());
+        }
+
     }
 
 }
