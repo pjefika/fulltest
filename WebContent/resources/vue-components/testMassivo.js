00546b16300c540a0a8fa3940375013e598c8468
FabioClem <fabio.henrique.clem@gmail.com>
2017-02-01 15:13:14 -0200

    Daily Commit: Teste

--- WebContent/resources/vue-components/testMassivo.js
+++ WebContent/resources/vue-components/testMassivo.js
@@ -4,4 +4,130 @@
  * and open the template in the editor.
  */
 
-var testURL = "";
+var loteURL = "/fulltestAPI/lote/massivo/";
+var instanciaURL = "/fulltestAPI/instancia/massivo/";
+new Vue({
+    el: "#massivo",
+    data: {
+        lotes: null,
+        adcLote: {
+            id: null,
+            status: null,
+            observacao: "Teste",
+            matricula: null
+        },
+        delLote: {
+            id: null,
+            status: null,
+            observacao: null,
+            matricula: null
+        },
+        instancias: "123;213;231;312;321",
+        delimiter: ";"
+    },
+    created: function () {
+        var self = this;
+        self.getLotes();
+    },
+    methods: {
+        dateFormat: function (h) {
+            return moment(h).format('DD/MM/YYYY');
+        },
+        //LIST
+        getLotes: function () {
+            var self = this;
+            $.get(loteURL + "ativos", function (data) {
+                self.lotes = data.list;
+                self.lotes = _.orderBy(self.lotes, ['dataCriacao'], ['asc'])
+            });
+        },
+        //ADC
+        createLote: function () {
+            var self = this;
+            $.ajax({
+                type: "POST",
+                url: loteURL + "cadastrar",
+                data: JSON.stringify(self.adcLote),
+                dataType: "json",
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader("Content-Type", "application/json");
+                },
+                success: function (data) {
+                    self.fetchLotes();
+                    self.createListaLinhas(data.lote);
+                }
+            });
+        },
+        createListaLinhas: function (h) {
+            var self = this;
+            var listI = self.instancias.split(self.delimiter);
+            var lt = h;
+            lt.tests = [];
+            for (var i = 0; i < listI.length; i++) {
+                lt.tests.push({id: null, status: null, instancia: listI[i], lote: {id: h.id}});
+            }
+            $.ajax({
+                type: "POST",
+                url: loteURL + "modificar",
+                data: JSON.stringify(lt),
+                dataType: "json",
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader("Content-Type", "application/json");
+                },
+                success: function () {
+                    self.reset();
+                    $("#adcLote").modal("hide");
+                }
+            });
+            //console.log(lt);
+        },
+        //Exclui
+        deleLote: function () {
+            var self = this;
+            $.ajax({
+                type: "POST",
+                url: loteURL + "excluir",
+                data: JSON.stringify(self.delLote),
+                dataType: "json",
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader("Content-Type", "application/json");
+                },
+                success: function () {
+                    self.reset();
+                    self.getLotes();
+                    $("#delLote").modal("hide");
+                }
+            });
+        },
+        fetchLotes: function () {
+            var self = this;
+            setTimeout(function () {
+                self.getLotes();
+            }, 600);
+        },
+        fetchLoteDel: function (h) {
+            var self = this;
+            self.delLote = h;
+        },
+        //Reset Objects
+        reset: function () {
+            var self = this;
+            self.adcLote = {
+                id: null,
+                status: null,
+                observacao: null,
+                matricula: null
+            };
+            delLote = {
+                id: null,
+                status: null,
+                observacao: null,
+                matricula: null
+            };
+            instancias: null;
+            delimiter: null;
+        }
+
+    }
+
+});
