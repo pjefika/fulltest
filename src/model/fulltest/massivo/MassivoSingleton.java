2c5fad6e2b493cd04116b58740c4044d528ee77d
Agibert <matheusagibert@gmail.com>
2017-03-27 20:00:52 -0300

    acessa Dao ok porem nao roda threads...

--- src/model/fulltest/massivo/MassivoSingleton.java
+++ src/model/fulltest/massivo/MassivoSingleton.java
@@ -7,14 +7,15 @@ package model.fulltest.massivo;
 
 import dao.massivo.LoteDAO;
 import dao.massivo.TesteClienteDAO;
-import java.util.Calendar;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import javax.ejb.Schedule;
+import java.util.concurrent.TimeUnit;
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
-import javax.ejb.Timeout;
+import javax.enterprise.concurrent.ManagedScheduledExecutorService;
 import javax.inject.Inject;
 import model.entity.TesteCliente;
 
@@ -22,6 +23,7 @@ import model.entity.TesteCliente;
  *
  * @author G0042204
  */
+
 @Singleton
 @Startup
 public class MassivoSingleton {
@@ -32,25 +34,28 @@ public class MassivoSingleton {
     @Inject
     private LoteDAO lDao;
 
-    @Timeout
-    public void timeOut() {
-        System.out.println(Calendar.getInstance().toString());
+    @Resource
+    private ManagedScheduledExecutorService scheduledExecutorService;
+
+    @PostConstruct
+    public void comecaAi() {
+        scheduledExecutorService.scheduleWithFixedDelay(this::abreThread, 1, 10, TimeUnit.SECONDS);
     }
-
-    @Schedule(second = "*/20", minute = "*/1", hour = "*")
+//    @Schedule(hour = "*", minute = "*", second = "*/20")
     public void abreThread() {
-
         System.out.println("model.fulltest.massivo.MassivoSingleton.abreThread()");
 
         Integer quantTest = 40;
 //        Integer quantThread = (quantTest-(quantTest/3));
         List<TesteCliente> l = dao.listarInstanciasPendentes(quantTest);
+        System.out.println("leLista : " + l.toString());
+        
         if (l.isEmpty()) {
             l = dao.listarInstanciasPresasExec(quantTest);
         }
 
         if (l != null) {
-            ExecutorService exec = Executors.newFixedThreadPool(quantTest);
+            ExecutorService exec = Executors.newCachedThreadPool();
 
             for (TesteCliente testeCliente : l) {
                 BackgroundTestThread b = new BackgroundTestThread(testeCliente, lDao, dao);
