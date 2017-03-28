fb9ec0b871c498ee4b638df8816f7a99b712033f
Agibert <matheusagibert@gmail.com>
2017-03-28 16:37:54 -0300

    criacao de threads atraves da controller, nao deploya

--- src/model/fulltest/massivo/LoteSingleton.java
+++ src/model/fulltest/massivo/LoteSingleton.java
@@ -5,46 +5,36 @@
  */
 package model.fulltest.massivo;
 
-import dao.massivo.LoteDAO;
-import java.util.Calendar;
-import java.util.List;
-import javax.ejb.Schedule;
-import javax.ejb.Singleton;
-import javax.ejb.Startup;
-import javax.inject.Inject;
-import model.entity.Lote;
-import model.fulltest.Status;
-
 /**
  *
  * @author G0042204
  */
-@Singleton
-@Startup
+//@Singleton
+//@Startup
 public class LoteSingleton {
 
-    @Inject
-    private LoteDAO lDao;
+//    @Inject
+//    private LoteDAO lDao;
 
     
-    @Schedule(second = "1", minute = "*/1", hour = "*")
-    public void loteStatus(){
-
-        List<Lote> ltExec = lDao.listarLotesEmExec();
-        for (Lote lote : ltExec) {
-            if(lote.isTestesConc()){
-                lote.setStatus(Status.CONCLUIDO);
-                lote.setDataFim(Calendar.getInstance());
-                try {
-                    lDao.editar(lote);    
-                } catch (Exception e) {
-                    System.out.println("paunolotepersist");
-                    e.printStackTrace();
-                }
-                
-            }
-        }
-        
-    }
+//    @Schedule(minute = "*/2", hour = "*")
+//    public void loteStatus(){
+//
+//        List<Lote> ltExec = lDao.listarLotesEmExec();
+//        for (Lote lote : ltExec) {
+//            if(lote.isTestesConc()){
+//                lote.setStatus(Status.CONCLUIDO);
+//                lote.setDataFim(Calendar.getInstance());
+//                try {
+//                    lDao.editar(lote);    
+//                } catch (Exception e) {
+//                    System.out.println("paunolotepersist");
+//                    e.printStackTrace();
+//                }
+//                
+//            }
+//        }
+//        
+//    }
 
 }
--- src/model/fulltest/massivo/MassivoSingleton.java
+++ src/model/fulltest/massivo/MassivoSingleton.java
@@ -5,72 +5,67 @@
  */
 package model.fulltest.massivo;
 
-import dao.massivo.LoteDAO;
-import dao.massivo.TesteClienteDAO;
-import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-import javax.annotation.PostConstruct;
-import javax.annotation.Resource;
-import javax.ejb.Singleton;
-import javax.ejb.Startup;
-import javax.enterprise.concurrent.ManagedScheduledExecutorService;
-import javax.inject.Inject;
-import model.entity.TesteCliente;
-
 /**
  *
  * @author G0042204
  */
-
-@Singleton
-@Startup
+//@Singleton
 public class MassivoSingleton {
-
-    @Inject
-    private TesteClienteDAO dao;
-
-    @Inject
-    private LoteDAO lDao;
-
-    @Resource
-    private ManagedScheduledExecutorService scheduledExecutorService;
-
-    @PostConstruct
-    public void comecaAi() {
-        scheduledExecutorService.scheduleWithFixedDelay(this::abreThread, 1, 10, TimeUnit.SECONDS);
-    }
-//    @Schedule(hour = "*", minute = "*", second = "*/20")
-    public void abreThread() {
-        System.out.println("model.fulltest.massivo.MassivoSingleton.abreThread()");
-
-        Integer quantTest = 40;
-//        Integer quantThread = (quantTest-(quantTest/3));
-        List<TesteCliente> l = dao.listarInstanciasPendentes(quantTest);
-        System.out.println("leLista : " + l.toString());
-        
-        if (l.isEmpty()) {
-            l = dao.listarInstanciasPresasExec(quantTest);
-        }
-
-        if (l != null) {
-            ExecutorService exec = Executors.newCachedThreadPool();
-
-            for (TesteCliente testeCliente : l) {
-                BackgroundTestThread b = new BackgroundTestThread(testeCliente, lDao, dao);
-                exec.execute(b);
-            }
-
-            exec.shutdown();
-
-            while (!exec.isTerminated()) {
-
-            }
-
-            System.out.println("Cabo as thread!");
-        }
-
-    }
+//
+//    @Inject
+//    private TesteClienteDAO dao;
+//
+//    @Inject
+//    private LoteDAO lDao;
+//
+////    @Resource
+////    private ManagedScheduledExecutorService scheduledExecutorService;
+//
+////    @PostConstruct
+////    public void comecaAi() {
+//////        scheduledExecutorService.scheduleWithFixedDelay(this::abreThread, 1, 10, TimeUnit.SECONDS);
+////        while (1 == 1) {
+////            Thread oi = new Thread(this::abreThread);
+////            oi.start();
+////            try {
+////                oi.join();
+////            } catch (InterruptedException ex) {
+////                Logger.getLogger(MassivoSingleton.class.getName()).log(Level.SEVERE, null, ex);
+////            }
+////        }
+////
+////    }
+////    @Schedule(hour = "*", minute = "*", second = "*/20")
+//
+//    public void abreThread() {
+//        System.out.println("model.fulltest.massivo.MassivoSingleton.abreThread()");
+//
+//        Integer quantTest = 40;
+////        Integer quantThread = (quantTest-(quantTest/3));
+//        List<TesteCliente> l = dao.listarInstanciasPendentes(quantTest);
+//        System.out.println("leLista : " + l.toString());
+//
+//        if (l.isEmpty()) {
+//            l = dao.listarInstanciasPresasExec(quantTest);
+//        }
+//
+//        if (l != null) {
+//            ExecutorService exec = Executors.newCachedThreadPool();
+//
+//            for (TesteCliente testeCliente : l) {
+//                BackgroundTestThread b = new BackgroundTestThread(testeCliente, lDao, dao);
+//                exec.execute(b);
+//            }
+//
+//            exec.shutdown();
+//
+//            while (!exec.isTerminated()) {
+//
+//            }
+//
+//            System.out.println("Cabo as thread!");
+//        }
+//
+//    }
 
 }
