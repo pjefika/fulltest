0d98c5ac608fa691e3a1447d82ebcecf6546b4f2
henmerlin <henmerlin@gmail.com>
2017-04-13 15:59:03 -0300

    Novas Validações - new branch

---
+++ src/model/validacao/ValidacaoRede.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.validacao;
+
+import bean.ossturbonet.oss.gvt.com.GetInfoOut;
+import com.gvt.www.ws.eai.oss.OSSTurbonetInconsistenciaTBSRadius.OSSTurbonetInconsistenciaTBSRadiusOut;
+
+/**
+ *
+ * @author G0042204
+ */
+public class ValidacaoRede extends Validacao {
+
+    private GetInfoOut info;
+
+    private OSSTurbonetInconsistenciaTBSRadiusOut i;
+
+    public ValidacaoRede(GetInfoOut info, OSSTurbonetInconsistenciaTBSRadiusOut i) {
+        this.info = info;
+        this.i = i;
+        this.nome = "Cadastro TBS";
+    }
+
+    /**
+     * Implementar!
+     *
+     * @return
+     */
+    @Override
+    public Boolean validar() {
+        return !i.getEhInconsistente() && info != null && info.getInfoTBS().getStatus().equalsIgnoreCase("ATIVO");
+    }
+}
