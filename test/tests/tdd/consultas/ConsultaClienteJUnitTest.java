6271e5da66d9b81d42975e5a30b2cdc7e4f3f2cd
Agibert <matheusagibert@gmail.com>
2017-04-13 13:03:51 -0300

    jar Funcionando

--- test/tests/tdd/consultas/ConsultaClienteJUnitTest.java
+++ test/tests/tdd/consultas/ConsultaClienteJUnitTest.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tdd.consultas;
+package tests.tdd.consultas;
 
 import java.rmi.RemoteException;
 import java.util.logging.Level;
