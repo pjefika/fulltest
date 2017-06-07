7cb638e21615ae2e9556c841829bf3b97ad76fec
Henrique Merlin <henmerlin@gmail.com>
2017-06-07 14:57:51 -0300

    inicio operacional

---
+++ src/model/fulltest/operacional/FullTestGpon.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.fulltest.operacional;
+
+import model.dslam.consulta.gpon.ConsultaGponDefault;
+
+/**
+ *
+ * @author G0042204
+ */
+public class FullTestGpon {
+    
+    private ConsultaGponDefault c;
+    
+    
+    
+}
