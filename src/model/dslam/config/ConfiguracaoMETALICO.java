5312e2c5c364d7ce302a6836ac2242f497e8df2e
Agibert <matheusagibert@gmail.com>
2017-11-27 14:22:25 -0200

    workaround class name - ConfiguracaoDSLAM

---
+++ src/model/dslam/config/ConfiguracaoMETALICO.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model.dslam.config;
+
+import model.validacao.impl.both.ValidacaoResult;
+
+/**
+ *
+ * @author G0042204
+ */
+public class ConfiguracaoMETALICO extends ConfiguracaoPorta {
+
+    private ValidacaoResult tabRede;
+    
+   
+    public ConfiguracaoMETALICO() {
+    }
+
+    public ValidacaoResult getTabRede() {
+        return tabRede;
+    }
+
+    public void setTabRede(ValidacaoResult tabRede) {
+        this.tabRede = tabRede;
+    }
+
+}
